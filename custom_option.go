76931ab87927ddb2a3d887e661b8482739330e09
Dmitrii Okunev <xaionaro@dx.center>
2025-03-16 23:58:21 +0000

    Add type alias for DictionaryItem

--- custom_option.go
+++ custom_option.go
@@ -5,3 +5,4 @@ import "github.com/xaionaro-go/avpipeline/types"
 type HardwareDeviceType = types.HardwareDeviceType
 type HardwareDeviceName = types.HardwareDeviceName
 type DictionaryItems = types.DictionaryItems
+type DictionaryItem = types.DictionaryItem
