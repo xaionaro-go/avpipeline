850662e7780fcdef76708ecc9915e8303037f89d
Dmitrii Okunev <xaionaro@dx.center>
2025-04-07 00:48:58 +0100

    Add SkipStreamIndexOutOfOrder

---
+++ packet/condition/extra/init_same_stream_order.go
@@ -0,0 +1,46 @@
+package extra
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/facebookincubator/go-belt/tool/logger"
+	"github.com/xaionaro-go/avpipeline/packet"
+	"github.com/xaionaro-go/avpipeline/packet/condition"
+	"github.com/xaionaro-go/xsync"
+)
+
+type SkipStreamIndexOutOfOrder struct {
+	ExpectingStreamIndexGE int
+	Locker                 xsync.Mutex
+}
+
+var _ condition.Condition = (*SkipStreamIndexOutOfOrder)(nil)
+
+func (i *SkipStreamIndexOutOfOrder) Match(
+	ctx context.Context,
+	pkt packet.Input,
+) bool {
+	return xsync.DoR1(ctx, &i.Locker, func() bool {
+		streamIndex := pkt.GetStreamIndex()
+		logger.Tracef(ctx, "curIdx:%d; expectedNotLower:%d", i.ExpectingStreamIndexGE)
+		switch {
+		case streamIndex < i.ExpectingStreamIndexGE:
+			return true
+		case streamIndex == i.ExpectingStreamIndexGE:
+			i.ExpectingStreamIndexGE++
+			return true
+		default:
+			logger.Tracef(ctx, "skipping a package out of order, because stream index %d is higher than %d", streamIndex, i.ExpectingStreamIndexGE)
+			return false
+		}
+	})
+}
+
+func (i *SkipStreamIndexOutOfOrder) String() string {
+	ctx := xsync.WithNoLogging(context.TODO(), true)
+	idx := xsync.DoR1(ctx, &i.Locker, func() int {
+		return i.ExpectingStreamIndexGE
+	})
+	return fmt.Sprintf("SkipStreamIndexOutOfOrder(accepting <= %d)", idx)
+}
--- types/condition.go
+++ types/condition.go
@@ -9,3 +9,18 @@ type Condition[T any] interface {
 	fmt.Stringer
 	Match(context.Context, T) bool
 }
+
+/* for easier copy&paste:
+
+func (c *) Match(
+	ctx context.Context,
+	pkt packet.Input,
+) bool {
+
+}
+
+func (c *) String() string {
+
+}
+
+*/
