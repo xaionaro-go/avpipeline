bcfd2ea2632de559ebe76e85154713dc476c2cab
Dmitrii Okunev <xaionaro@dx.center>
2025-05-11 00:36:11 +0100

    Add 'In' condition for 'Node'

---
+++ node/condition/in.go
@@ -0,0 +1,37 @@
+package condition
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/xaionaro-go/avpipeline/node"
+)
+
+type In []node.Abstract
+
+var _ Condition = (In)(nil)
+
+func (s In) String() string {
+	var result []string
+	for _, n := range s {
+		if stringer, ok := n.(fmt.Stringer); ok {
+			result = append(result, stringer.String())
+		} else {
+			result = append(result, fmt.Sprintf("%T", n))
+		}
+	}
+	return fmt.Sprintf("(%s)", strings.Join(result, "|"))
+}
+
+func (s In) Match(
+	ctx context.Context,
+	node node.Abstract,
+) bool {
+	for _, item := range s {
+		if node == item {
+			return true
+		}
+	}
+	return false
+}
